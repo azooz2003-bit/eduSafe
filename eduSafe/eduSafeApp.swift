a00d8036d0c25d09228fb2bae4a041f37f14346f
Abdulaziz Albahar <67667005+azooz2003-bit@users.noreply.github.com>
2022-10-14 21:24:41 -0400

    Initial Commit

---
+++ eduSafe/eduSafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  eduSafeApp.swift
+//  eduSafe
+//
+//  Created by Abdulaziz Albahar on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct eduSafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
