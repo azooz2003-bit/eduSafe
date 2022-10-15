d3d91e998fddd051d7ea1810cf61f8a98b566dc8
Addison Goolsbee <addisongoolsbee@gmail.com>
2022-10-15 10:46:18 -0400

    set up UVC basic stuff and comments for which functions

---
+++ eduSafe/Model + Controller/SchoolViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  SchoolViewModel.swift
+//  eduSafe
+//
+//  Created by Addison Goolsbee on 10/15/22.
+//
+
+import Foundation
+
+// needed functions
+// locationToMap: takes in coordinate (and elevation) and places it on the map image
+// displayAlerts: iterates through list of school's alerts and places them on map image
+//  printAlert: makes a visual alert thing and puts it on the screen (over the map)
+// zoom/swipe functions?
+
+// createAlert: after a user presses the alert button, make new alert object in user and in school
+
+
+
+// later functions
+// ?go to next floor: switches the current map view
+
+
